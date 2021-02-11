335f9a65d52588aad95901a362c301631dd551f1
MykhailoShykun <62407992+MykhailoShykun@users.noreply.github.com>
2021-02-12 01:24:18 +0200

    12.02.2021

---
+++ components/front-page-about-us/front-page-about-us.php
@@ -0,0 +1,15 @@
+<section class="section section_front-page-about-us">
+    <div class="front-page-about-us">
+        <div class="marquee">
+            <div class="marquee__inner">
+	            <?php for ($i = 0; $i < 20; $i++) { ?>
+                    <div class="circle"></div>
+	            <?php } ?>
+            </div>
+        </div>
+        <div class="container">
+            <hr>
+            <p class="text-block">With krug agency, your dream goes far beyond the limits of the imagination. With unequalled passion we break new ground to make a moment last a lifetime. With dazzling stories, technology, and artistry, we connect people and emotions to places.</p>
+        </div>
+    </div>
+</section>
--- front-page.php
+++ front-page.php
@@ -2,6 +2,7 @@
 <main class="main">
     <?php
     get_template_part('components/front-page-intro/front-page-intro');
+    get_template_part('components/front-page-about-us/front-page-about-us');
     ?>
 </main>
 <?php get_footer(); ?>
