1bedb65257eea2b93f5ddd0f6ff7b81472febce8
MykhailoShykun <62407992+MykhailoShykun@users.noreply.github.com>
2021-02-14 02:26:34 +0200

    14.02.2021

--- archive.php
+++ archive.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
-
+	<main class="main">
+		<?php
+		get_template_part('components/archive-posts-list/archive-posts-list');
+		?>
+	</main>
 <?php get_footer(); ?>
---
+++ components/archive-posts-list/archive-posts-list.php
@@ -0,0 +1,45 @@
+<section class="section section_archive-posts-list">
+    <div class="archive-posts-list">
+        <div class="main-post">
+            <h1 class="title">DESIGN</h1>
+            <div class="thumbnail-wrapper">
+                <img src="<?= get_template_directory_uri(); ?>/img/dest/thumbnail-sample.jpg" alt="thumbnail" class="main-post__thumbnail">
+                <?php if (wpmd_is_device()) { ?>
+                    <div class="button-wrapper">
+                        <img src="<?= get_template_directory_uri(); ?>/img/dest/new_mobile.png" alt="new" class="button__image">
+                        <img src="<?= get_template_directory_uri(); ?>/img/dest/new-triangle_mobile.jpg" alt=">" class="button__triangle">
+                    </div>
+                <?php } else { ?>
+                    <div class="button-wrapper">
+                        <img src="<?= get_template_directory_uri(); ?>/img/dest/new_desktop.png" alt="new" class="button__image">
+                        <img src="<?= get_template_directory_uri(); ?>/img/dest/new-triangle_mobile.jpg" alt=">" class="button__triangle">
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="container">
+                <p class="main-post__title">Название Проекта</p>
+                <p class="main-post__subtitle">Bring us your toughest business goals, your Bring us your toughest business goals, your tightest parameters.</p>
+            </div>
+        </div>
+        <div class="container">
+            <?php for ($i = 0; $i < 5; $i++) { ?>
+                <div class="post">
+                    <img src="<?= get_template_directory_uri(); ?>/img/dest/thumbnail-sample.jpg" alt="thumbnail" class="post__thumbnail">
+                    <a href="#" class="post__title">Название Проекта</a>
+                    <p class="post__subtitle">Bring us your toughest business goals, your Bring us your toughest business goals, your tightest parameters.</p>
+                </div>
+            <?php } ?>
+        </div>
+        <img src="<?= get_template_directory_uri(); ?>/img/dest/arrow-to-the-top.png" alt="to the top" class="arrow-to-top">
+        <script>
+            let arrow = document.querySelector('.arrow-to-top');
+
+            arrow.addEventListener('click', () => {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                })
+            })
+        </script>
+    </div>
+</section>
